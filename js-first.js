c5c562ecbe65214ac71ce66d4abfa6fd34fbcbe0
ashif <ashif@bititude.com>
2017-11-06 15:36:27 +0530

    Please enter the c mmit message for your changes. Lines starting

---
+++ js-first.js
@@ -0,0 +1,31 @@
+/*
+ * This is a JavaScript Scratchpad.
+ *
+ * Enter some JavaScript, then Right Click or choose from the Execute Menu:
+ * 1. Run to evaluate the selected text (Cmd-R),
+ * 2. Inspect to bring up an Object Inspector on the result (Cmd-I), or,
+ * 3. Display to insert the result in a comment after the selection. (Cmd-L)
+ */
+
+function first(array, number) {
+  if (number == null){
+    return array[0];
+  }
+  else if (number<0){
+    return [];
+  }
+  
+  else 
+  {
+    return array.slice(0, number);
+  }
+}
+
+console.log(first([7, 9, 0, -2]));
+console.log(first([],3));
+console.log(first([7, 9, 0, -2],3));
+console.log(first([7, 9, 0, -2],6));
+console.log(first([7, 9, 0, -2],-3));
+
+
+
