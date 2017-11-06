4b73145a9b096ed7acf7c9d1dbc84b9f5c48e361
ashif <ashif@bititude.com>
2017-11-06 15:52:54 +0530

    code for returning last element.

---
+++ js-last.js
@@ -0,0 +1,23 @@
+/*
+ * This is a JavaScript Scratchpad.
+ *
+ * Enter some JavaScript, then Right Click or choose from the Execute Menu:
+ * 1. Run to evaluate the selected text (Cmd-R),
+ * 2. Inspect to bring up an Object Inspector on the result (Cmd-I), or,
+ * 3. Display to insert the result in a comment after the selection. (Cmd-L)
+ */
+
+function last(array, number) {
+  if (number==null){
+    return array[array.length-1];
+  }
+  
+  else { 
+    return array.slice(-number);}
+}
+
+console.log(last([7, 9, 0, -2]));
+console.log(last([7, 9, 0, -2],3));
+console.log(last([7, 9, 0, -2],6));
+
+
