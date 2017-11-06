2019b0247a567c31eaba7fc5974788fbf7feb11a
ashif <ashif@bititude.com>
2017-11-06 16:39:09 +0530

    code to join string within a array

---
+++ js-joinstring.js
@@ -0,0 +1,20 @@
+/*
+ * This is a JavaScript Scratchpad.
+ *
+ * Enter some JavaScript, then Right Click or choose from the Execute Menu:
+ * 1. Run to evaluate the selected text (Cmd-R),
+ * 2. Inspect to bring up an Object Inspector on the result (Cmd-I), or,
+ * 3. Display to insert the result in a comment after the selection. (Cmd-L)
+ */
+
+myColor = ["Red", "Green", "White", "Black"];
+console.log(myColor.join());
+console.log(myColor.toString());
+console.log(myColor.join('+'));
+
+
+
+/*
+Exception: SyntaxError: expected expression, got ')'
+@Scratchpad/2:13
+*/
