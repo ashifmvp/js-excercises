27978e0c3ad5a73884971a650163ea7e10dcd1b3
ashif <ashif@bititude.com>
2017-11-08 14:15:22 +0530

    Please enter the commit message for your changes.Ly array members ines starting

---
+++ js-showarraymembers.js
@@ -0,0 +1,18 @@
+ function showArray(array){
+  
+for (var i=0; i<array.length; i++){
+  
+  console.log("row" +i);
+  
+  for (var j=0; j<array[i].length; j++){
+   
+    console.log(array[i][j]);
+    
+  }  
+}
+
+}
+
+showArray([[1, 2, 1, 24], [8, 11, 9, 4], [7, 0, 7, 27], [7, 4, 28, 14], [3, 10, 26, 7]]);   
+
+
